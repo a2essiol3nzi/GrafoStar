e360e42154c436bae0cf208ead2cb571359c2eb9
Alessio Lenzi <alenzi.CS@gmail.com>
2025-06-24 10:43:25 +0200

    Corretta scansione e produzione dell'intero file dei nomi

--- CreaGrafo.java
+++ CreaGrafo.java
@@ -1,4 +1,5 @@
 import java.util.Map;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -23,20 +24,17 @@ public class CreaGrafo {
     Map<Integer,Attore> actors = new HashMap<Integer,Attore>();
 
     try{
-      
       // apro name.tsv in lettura con buffer da 64KB (invece che da 8KB come di default)
-      // apro nomi.txt in scrittura con buffer da 64KB
         /* la scelta è motivata dalla grandezza dei file, per minimizzare il numero di accessi al disco il modulo Buffer
         usa già una sua bufferizzazione da 8KB, la facciamo da 64KB per ottimizzare: 2^10 * 2^6= 65536 */
       BufferedReader br = new BufferedReader(new FileReader(args[0]), 65536);
-      BufferedWriter bw = new BufferedWriter(new FileWriter("nomi.txt"), 65536);
       String line;
       
       while((line = br.readLine())!=null) {
         // verifico che la linea sia valida (anno di nascita e professione) se lo è allora aggiorno nomi.txt e actors
         String[] campi = line.split("\t");
-        for(String c: campi) 
-          c = c.trim();
+        for (int i = 0; i < campi.length; i++)
+          campi[i] = campi[i].trim();
         // verifico le condizioni e agisco di conseguenza
         if(!campi[2].equals("\\N")){
           String[] professions = campi[4].split(",");
@@ -45,18 +43,28 @@ public class CreaGrafo {
               // entrambe le cond verificate-> creiamo un nuovo Attore
               Attore a = new Attore(
                 Integer.parseInt(campi[0].substring(2)), campi[1], Integer.parseInt(campi[2]));
-              // scrivo nomi.txt: codice  nome  anno\n
-              bw.write(a + "\n");
               // aggiorno actors
               actors.put(a.codice, a);
+              break;
             }
           }
         }
         // se una delle cond non è verificata non faccio niente e passo alla prossima linea
       }
-
-      // chiudo i file aperti che ho finito di leggere/scrivere
+      // chiudo file name.basics.tsv
       br.close();
+
+      // si passa alla scrittura di nomi.txt
+      BufferedWriter bw = new BufferedWriter(new FileWriter("nomi.txt"), 65536);
+      // i nomi devono essere scritti in ordine crescente di codice -> riordino le chiavi in HashMap e poi da ognuna ricavo l'attore
+      // creo un array con tutte le chiavi della HashMap (codici degli attori validi) e le riordino
+      ArrayList<Integer> codici = new ArrayList<>(actors.keySet());
+      codici.sort((a,b)-> a-b);
+      // stampo ogni attore associato alle chiavi
+      for(Integer i: codici){
+        bw.write(actors.get(i).toString() + "\n");
+      }
+      // al termine richiudo il file
       bw.close();
       
     } catch(Exception e) {
@@ -65,7 +73,7 @@ public class CreaGrafo {
       System.exit(2);
     }
 
-
+    // punti 4-5-6
 
 
 
